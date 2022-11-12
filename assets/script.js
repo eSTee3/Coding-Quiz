a059bbfb660e732441090515672bc358f30b5c06
eSTee3 <tagsxa@yahoo.com>
2022-11-11 18:34:46 -0600

    Fixed results.js calls

--- assets/script.js
+++ assets/script.js
@@ -195,7 +195,7 @@ function allDone() {
             var newScore = JSON.stringify(allScores);
             localStorage.setItem("allScores", newScore);
             // Travels to final page
-            window.location.replace("./HighScores.html");
+            window.location.replace("./results.js");
         }
     });
 

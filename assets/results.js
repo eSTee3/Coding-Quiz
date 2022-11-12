a4a84935ba1fd5bea661b2a5a120cbba8fe7449f
eSTee3 <tagsxa@yahoo.com>
2022-11-12 09:08:56 -0600

    updated results.js with comments

--- assets/results.js
+++ assets/results.js
@@ -1,24 +1,28 @@
+// Variables
 var highScore = document.querySelector("#highScore");
 var clear = document.querySelector("#resetScores");
 var startOver = document.querySelector("#startOver");
  
+// Event Listener to clear the end user's local storage
 clear.addEventListener("click", function () {
     localStorage.clear();
     location.reload();
 });
 
+// variable containing all high scores in local storage
 var allScores = localStorage.getItem("allScores");
+
 allScores = JSON.parse(allScores);
 
+// Gathers and displays the list of highscores (if not cleared)
 if (allScores !== null) {
-
     for (var i = 0; i < allScores.length; i++) {
-
         var createLi = document.createElement("li");
         createLi.textContent = allScores[i].initials + " " + allScores[i].score;
         highScore.appendChild(createLi);
     }
 }
+
 startOver.addEventListener("click", function () {
     // the 2 dots before index.html force the code to go up a folder level
     window.location.replace("../index.html");
